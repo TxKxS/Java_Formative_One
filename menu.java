91b47eeaae05446f26a63f46bb527bb4f7a88404
tks <t.sauntoo@alustudent.com>
2026-08-07 22:15:00 +0400

    First commit

---
+++ menu.java
@@ -0,0 +1,20 @@
+public class menu {
+    public static void main (String[] args) {
+
+        chat menuChoice = '';
+
+        //menu
+        System.out.println("REKOLT PRODUCE TRACKER  -  season 2026")
+        System.out.println("1. Record a delivery")
+        System.out.println("2. Season figures on screen")
+        System.out.println("3. Generate the season report")
+        System.out.println("4. Exit")
+
+        //Ask for choice
+
+
+
+
+
+    }
+}
