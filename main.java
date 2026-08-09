49d8ff1fc3f0688961e3e91b0e47e07a55be408d
tks <t.sauntoo@alustudent.com>
2026-08-09 13:47:48 +0400

    Created Menu Obj and using it to display the menu from anotheer file in the same repo

--- main.java
+++ main.java
@@ -2,25 +2,13 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
+        menu myMenu = new menu();
 
-    }
-
-    public char menu(){
+        int choic = myMenu.menu();
 
 
-        chat menuChoice = '';
-
-        //menu
-        System.out.println("REKOLT PRODUCE TRACKER  -  season 2026");
-        System.out.println("1. Record a delivery");
-        System.out.println("2. Season figures on screen");
-        System.out.println("3. Generate the season report");
-        System.out.println("4. Exit");
-
-        //Ask for choice
 
 
     }
-
 
 }
--- menu.java
+++ menu.java
@@ -1,20 +1,25 @@
 public class menu {
-    public static void main (String[] args) {
 
-        chat menuChoice = '';
+    //import sanner something, will look it up later
+
+    public int menu () {
+
+
+        int menuChoice = 0;
 
         //menu
-        System.out.println("REKOLT PRODUCE TRACKER  -  season 2026")
-        System.out.println("1. Record a delivery")
-        System.out.println("2. Season figures on screen")
-        System.out.println("3. Generate the season report")
-        System.out.println("4. Exit")
+        System.out.println("REKOLT PRODUCE TRACKER  -  season 2026");
+        System.out.println("1. Record a delivery");
+        System.out.println("2. Season figures on screen");
+        System.out.println("3. Generate the season report");
+        System.out.println("4. Exit");
 
         //Ask for choice
+        System.out.print("Please enter your choice: ");
 
+        //input choise
 
-
-
+        return menuChoice;
 
     }
 }
