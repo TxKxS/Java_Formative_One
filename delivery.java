f2da6825c6dd05734b2be7565ac251e323414912
tks <t.sauntoo@alustudent.com>
2026-08-10 11:53:38 +0400

    Added devliery

---
+++ delivery.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class delivery{
+
+    public static void delivery() {
+
+
+        System.out.println("Record a Delivery");
+
+    }
+}
--- main.java
+++ main.java
@@ -5,7 +5,21 @@ public class main {
 
         System.out.println("Option " + menuChoice + " chosen");
 
+        switch (menuChoice) {
+            case '1':
+                delivery.delivery();
 
+                break;
+            case '2':
+
+                break;
+            case '3':
+
+                break;
+            case '4':
+                System.out.print("Thank you for using TKS's program!");
+                System.exit(0);
+        }
 
     }
 
