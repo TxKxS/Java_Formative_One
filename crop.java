f72e813d139c5b4efcf4191a07a8ff3070a38ba6
tks <t.sauntoo@alustudent.com>
2026-08-17 11:29:15 +0400

    Crop.java should be completed now. Added getter methods

--- crop.java
+++ crop.java
@@ -1,13 +1,34 @@
 public class crop{
 
+    //interface
+
     private float fltWeight = 0;
     private char chrGrade = 'Z';
     private String strProduceCode = "None";
     private String strCategory = "None";
+    private float fltPrice = 0;
+
+    public static int getPrice(){
+        return this.fltPrice;
+    }
+
+    public static char getGrade(){
+        return this.chrGrade;
+    }
+
+    public static String getProduceCode(){
+        return this.stProduceCode;
+    }
+
+    public static String getCategory(){
+        return this.strCategory;
+    }
 
 }
 
 public class Maize extends crop{
+
+    private final int price = 30 // Rs30/Kg
 
     public class Maize(float Weight, char Grade, String ProduceCode, String Category) {
             this.fltWeight = Weight;
@@ -19,6 +40,8 @@ public class Maize extends crop{
 }
 
 public class Beans extends crop {
+
+    private final int price = 90 // Rs90/Kg
 
     public class Beans(float Weight, char Grade, String ProduceCode, String Category) {
             this.fltWeight = Weight;
@@ -32,6 +55,8 @@ public class Beans extends crop {
 
 public class Potatoes extends crop {
 
+    private final int price = 45 // Rs45/Kg
+
     public class potatoes(float Weight, char Grade, String ProduceCode, String Category) {
             this.fltWeight = Weight;
             this.chrGrade = Grade;
@@ -43,6 +68,8 @@ public class Potatoes extends crop {
 }
 
 public class GreenTea extends crop {
+
+    private final int price = 25 // Rs25/Kg
 
     public class GreenTea(float Weight, char Grade, String ProduceCode, String Category) {
             this.fltWeight = Weight;
