db23e36c003249a866062077c376004364427ae9
tks <t.sauntoo@alustudent.com>
2026-08-24 22:20:59 +0400

    Added verification for all inputs and clearing out buffer for input of input object

--- delivery.java
+++ delivery.java
@@ -5,6 +5,8 @@ public class delivery{
     public static void delivery(Scanner input) {
 
         float fltMass = 0;
+        int intQualityScore = 0;
+        int intWeekOfDelivery;
 
 
         System.out.println("Record a Delivery");
@@ -33,20 +35,48 @@ public class delivery{
         //Record Mass
         do {
             System.out.print("Mass (in Kg): ");
-            fltMass = input.nextFloat();
-            if (fltMass < 0) {
-                System.out.println("Mass cannot be negative. It needs to be greater than 0.");
-            } else if (fltMass > 5000) {
-                System.out.println("Maximum mass is only 5000.");
+            try {
+                fltMass = input.nextFloat();
+                if (fltMass < 0) {
+                    System.out.println("Mass cannot be negative. It needs to be greater than 0.");
+                } else if (fltMass > 5000) {
+                    System.out.println("Maximum mass is only 5000 (<5000).");
+                }
+            } catch (Exception e) {
+                System.out.println("Please enter a 'number' between 0 and 5000.");
+                input.next(); //Clearing up the buffer to remove the infite loop bug
+                fltMass = -1;
             }
         } while (fltMass < 0 || fltMass > 5000);
 
+        do {
+            System.out.print("Quality Score (0-100): ");
+            try {
+                intQualityScore = input.nextInt();
+                if (intQualityScore < 0 || intQualityScore > 100) {
+                    System.out.println("Invalid Quality Score. It needs to be between 0 and 100 (inclusive).");
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid Quality Score. Please enter an integer between 0 and 100 inclusive.");
+                input.next(); //Clearing up the buffer to remove the infite loop bug
+                intQualityScore = -1;
+            };
+        } while (intQualityScore < 0 || intQualityScore > 100);
 
-        System.out.print("Quality Score (0-100): ");
-        int intQualityScore = input.nextInt();
-
-        System.out.print("Week of Delivery (1-20): ");
-        int intWeekOfDelivery = input.nextInt();
+        do {
+            try {
+                System.out.print("Week of Delivery (1-20): ");
+                intWeekOfDelivery = input.nextInt();
+                if (intWeekOfDelivery < 1 || intWeekOfDelivery > 20) {
+                    System.out.println("Invalid option. Please enter a number between 1 and 20.");
+                }
+                ;
+            } catch (Exception e) {
+                System.out.println("Invalid option. Please enter an integer between 1 and 20.");
+                input.next(); //Clearing up the buffer
+                intWeekOfDelivery = -1;
+            }
+        } while (intWeekOfDelivery < 1 || intWeekOfDelivery > 20);
 
     }
 
