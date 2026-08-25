ed30e499e247e48387b20bd2bd055ba254cbba01
tks <t.sauntoo@alustudent.com>
2026-08-25 21:00:50 +0400

    Converted cro class to an abstract class and added repetitive function to Super(crop) class itself

--- crop.java
+++ crop.java
@@ -6,20 +6,31 @@ public abstract class crop{
     private String strProduceCode = "None";
     private String strCategory = "None";
     private float fltPrice = 0;
+    private int intDeliveryWeek = 0;
+
+    public class crop(float Weight, char Grade, String ProduceCode, String Category,  int intDeliveryWeek) {
+            this.fltWeight = Weight;
+            this.chrGrade = Grade;
+            this.strProduceCode = ProduceCode;
+            this.strCategory = Category;
+            this.intDeliveryWeek = intDeliveryWeek;
+    }
+
+
 
     public static int getPrice(){
         return this.fltPrice;
     }
 
-    public static char getGrade(){
+    public char getGrade(){
         return this.chrGrade;
     }
 
-    public static String getProduceCode(){
+    public String getProduceCode(){
         return this.stProduceCode;
     }
 
-    public static String getCategory(){
+    public String getCategory(){
         return this.strCategory;
     }
 
@@ -27,40 +38,30 @@ public abstract class crop{
 
 public class Maize extends crop{
 
-    private final int price = 30 // Rs30/Kg
+    private final int price = 30; // Rs30/Kg
 
-    public class Maize(float Weight, char Grade, String ProduceCode, String Category) {
-            this.fltWeight = Weight;
-            this.chrGrade = Grade;
-            this.strProduceCode = ProduceCode;
-            this.strCategory = Category;
+    public class Maize(float Weight, char Grade, String ProduceCode, String Category, int intDeliveryWeek) {
+            super(Weight,Grade, ProduceCode,Category, intDeliveryWeek)
     }
 
 }
 
 public class Beans extends crop {
 
-    private final int price = 90 // Rs90/Kg
+    private final int price = 90; // Rs90/Kg
 
-    public class Beans(float Weight, char Grade, String ProduceCode, String Category) {
-            this.fltWeight = Weight;
-            this.chrGrade = Grade;
-            this.strProduceCode = ProduceCode;
-            this.strCategory = Category;
+    public class Beans(float Weight, char Grade, String ProduceCode, String Category, int intDeliveryWeek) {
+            super(Weight,Grade, ProduceCode,Category, intDeliveryWeek)
     }
-
 
 }
 
 public class Potatoes extends crop {
 
-    private final int price = 45 // Rs45/Kg
+    private final int price = 45; // Rs45/Kg
 
-    public class Potatoes(float Weight, char Grade, String ProduceCode, String Category) {
-            this.fltWeight = Weight;
-            this.chrGrade = Grade;
-            this.strProduceCode = ProduceCode;
-            this.strCategory = Category;
+    public class Potatoes(float Weight, char Grade, String ProduceCode, String Category, int intDeliveryWeek) {
+            super(Weight,Grade, ProduceCode,Category, intDeliveryWeek)
     }
 
 
@@ -68,15 +69,11 @@ public class Potatoes extends crop {
 
 public class GreenTea extends crop {
 
-    private final int price = 25 // Rs25/Kg
+    private final int price = 25; // Rs25/Kg
 
-    public class GreenTea(float Weight, char Grade, String ProduceCode, String Category) {
-            this.fltWeight = Weight;
-            this.chrGrade = Grade;
-            this.strProduceCode = ProduceCode;
-            this.strCategory = Category;
+    public class GreenTea(float Weight, char Grade, String ProduceCode, String Category, int intDeliveryWeek) {
+            super(Weight,Grade, ProduceCode,Category, intDeliveryWeek)
     }
-
 
 }
 
